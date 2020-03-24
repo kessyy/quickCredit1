bff77bfb92f2a155fb3d0cd348975ac4a56d2c00
kessyy <kather946@gmail.com>
2020-03-24 12:46:42 +0300

    remove error files

--- api/src/routes/loan.js
+++
@@ -1,9 +0,0 @@
-const loanController = require('../../controllers/loan');
-const express = require('express');
-const router = express.Router();
-
-
-router.post('/loans', loanController.createLoan,);
-
-  
-module.exports = router;
